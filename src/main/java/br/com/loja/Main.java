71473539ce3278d9ed0ffb980c4c326494bfd57c
Kaio Richard <kaio_r_lisboa@estudante.sesisenai.org.br>
2026-09-17 20:17:49 -0300

    feat: adiciona migracao de pedidos

--- src/main/java/br/com/loja/Main.java
+++ src/main/java/br/com/loja/Main.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import br.com.loja.conexao.Conexao;
-import br.com.loja.migracao.CarrinhosMigracao;
+import br.com.loja.migracao.PedidosMigracao;
 
 
 public class Main {
@@ -16,7 +16,7 @@ public class Main {
 
         System.out.println("Conectado!");
 
-        CarrinhosMigracao migracao3 = new CarrinhosMigracao();
+        PedidosMigracao migracao3 = new PedidosMigracao();
 
         System.out.println("=== MIGRANDO PRODUTOS IMAGENS ===");
         migracao3.migrar(conexao);
---
+++ src/main/java/br/com/loja/migracao/PedidoItensMigracao.java
@@ -0,0 +1,5 @@
+package br.com.loja.migracao;
+
+public class PedidoItensMigracao {
+    
+}
